0ce033025d73dfd2108ae86ef8ee1e0ee7bb73ef
claudiazeppieri <claudia.zeppieri@outlook.com>
2019-03-16 21:39:07 -0400

    Started index.html

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function pickpostal() {
+    var = document.getElementById(pickpostal);
+    if (x.style.display === Canada) {
+        x.style.display = "show";
+    } else {
+        x.style.display = "block";
+    }
+
+}
